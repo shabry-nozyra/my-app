3b67067c3130c1e6ded2fdafeb2dfdfd9b4bbadf
shabry-nozyra <shabry@nozyra.com>
2021-03-23 15:01:32 +0700

    ubah navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -142,7 +142,7 @@ export const Navbar = () => {
             <li className="nav-item dropdown no-arrow">
                 <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span className="mr-2 d-none d-lg-inline text-gray-600 small">@Nama_adminku</span>
+                    <span className="mr-2 d-none d-lg-inline text-gray-600 small">@Nama_admin</span>
                     <img className="img-profile rounded-circle"
                         src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"></img>
                 </a>
