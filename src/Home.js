879011320349a4f091bd55165fdb61d1fb36f374
shabry-nozyra <shabry@nozyra.com>
2021-03-25 08:06:25 +0700

    ubah home.js

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ function Home() {
     return (
         <div className="">
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 className="h3 mb-0 text-gray-800">HALAMAN DEPAN</h1>
+                <h1 className="h3 mb-0 text-gray-800">HALAMAN DEPAN2</h1>
                 <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                    className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                    className="fas fa-download fa-sm text-white-50"></i> Generate Report2</a>
             </div>
             <div className="row pt-0">
                 <div className="col-12 header-bar">
